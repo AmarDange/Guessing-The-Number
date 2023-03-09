a07f2128fb869853db8ad705f46e52312e4e36e0
AmarDange <amardange218@gmail.com>
2023-03-09 18:58:47 +0000

    final code

--- run.py
+++ run.py
@@ -6,13 +6,15 @@ def guess(x):
     random_number = random.randint(1, x)
     user_guess = None
     guess_count = 0
-    while user_guess!=random_number:
-        user_guess = int(input(f"Guess the number between 1 to {x} - \n"))
+    while user_guess != random_number:
+        user_guess = int(input(f"Guess the number between 1 to {x}  -  \n"))
+
         guess_count+=1
+
         if user_guess > random_number:
-            print("Sorry, It's too high. Try Again")
+                print("Sorry, It's too high. Try Again")
         elif user_guess < random_number:
-            print("Sorry, It's too low. Try Again")
-        print(f"Congrats... You Guess The Correct Number in {guess_count} times.")
+                print("Sorry, It's too low. Try Again")
+    print(f"Congrats... You Guess The Correct Number in {guess_count} times.")
 
 guess(101) 
